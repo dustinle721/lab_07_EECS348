063bc742ea26c2090174560ba97a25c57456d841
dustinle721 <157640407+dustinle721@users.noreply.github.com>
2024-04-04 14:56:45 -0500

    updated practice3.js

--- practice3.js
+++ practice3.js
@@ -1,4 +1,25 @@
-//grab the value of password (p1)
-//grab the value of reentered password (p2)
-//check if the length of p1>8 && p2 >8 
-//then check if they match 
+function revealPassword(inputID) {
+    var input = document.getElementById(inputID);
+    if (input.type === "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
+
+function validatePasswords() {
+    var password1 = document.getElementById("password_1").value;
+    var password2 = document.getElementById("password_2").value;
+
+    if (password1.length < 8 || password2.length < 8) {
+        alert("Password must be at least eight characters long!");
+        return;
+    }
+
+    if (password1 !== password2) {
+        alert("Passwords do not match!");
+        return;
+    }
+
+    alert("Passwords match!");
+}
